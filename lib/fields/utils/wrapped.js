ismaelga/react-json-editor

commit faaff6e1b70da4eccf7ba41870e78cb605439850

Split up /lib
I split up fields to separate files. Further notes at the source.

Note that it seems to be possible that `this.props.type` is not defined
for Selection always (ie. for enums). I made it to default to string in
this case. Perhaps there is a nicer way to fix this at a higher level.

lib/fields/utils/wrapped.js
@@ -1,0 +1,91 @@
+'use strict';
+
+var React = require('react');
+var $ = React.DOM;
+
+var ou = require('plexus-objective');
+
+
+var errorClass = function(errors) {
+  return (errors === null || errors.length === 0) ? '' : 'error';
+};
+
+var makeTitle = function(description, errors) {
+  var parts = [];
+  if (description !== null && description.length > 0) {
+    parts.push(description);
+  }
+  if (errors !== null && errors.length > 0) {
+    parts.push(errors.join('\n'));
+  }
+  return parts.join('\n\n');
+};
+
+var FieldWrapper = React.createClass({
+  render: function() {
+    var classes = [].concat(errorClass(this.props.errors) || [],
+                            'form-element',
+                            this.props.classes || []);
+
+    return $.div(
+      {
+        className: classes.join(' '),
+        key      : this.props.key,
+        title    : makeTitle(this.props.description, this.props.errors)
+      },
+      $.label(
+        {
+          htmlFor: this.props.key
+        },
+        this.props.title),
+      this.props.children);
+  }
+});
+
+var SectionWrapper = React.createClass({
+  render: function() {
+    var level = this.props.path.length;
+    var classes = [].concat('form-section',
+                            (level > 0 ? 'form-subsection' : []),
+                            this.props.classes || []);
+    var legendClasses = [].concat(errorClass(this.props.errors) || [],
+                                  'form-section-title');
+
+    return $.fieldset(
+      {
+        className: classes.join(' '),
+        key      : this.props.key
+      },
+      $.legend(
+        {
+          className: legendClasses.join(' '),
+          title    : makeTitle(this.props.description, this.props.errors)
+        },
+        this.props.title),
+      this.props.children);
+  }
+});
+
+
+var propsForWrapper = function(props) {
+  return {
+    key        : props.key,
+    path       : props.path,
+    errors     : props.errors,
+    classes    : ou.getIn(props.schema, ['x-hints', 'form', 'classes']),
+    title      : props.schema.title,
+    description: props.schema.description
+  };
+};
+
+exports.section = function(props, fields) {
+  return React.createElement(props.sectionWrapper || SectionWrapper,
+    propsForWrapper(props),
+    fields);
+};
+
+exports.field = function(props, field) {
+  return React.createElement(props.fieldWrapper || FieldWrapper,
+    propsForWrapper(props),
+    field);
+};

lib/alternative.js
@@ -1,0 +1,25 @@
+'use strict';
+
+var ou = require('plexus-objective');
+
+
+exports.schema = function(value, schema) {
+  var selector, options, selected;
+
+  selector = ou.getIn(schema, ['x-hints', 'form', 'selector']);
+  if (!selector) {
+    return;
+  }
+
+  options = schema.oneOf.map(function(alt) {
+    return ou.getIn(alt, [ 'properties', selector, 'enum', 0 ]) || "";
+  });
+
+  selected = (value || {})[selector] || options[0];
+
+  return ou.merge(ou.setIn(schema.oneOf[options.indexOf(selected)],
+                           [ 'properties', selector ],
+                           ou.merge(ou.getIn(schema, [ 'properties', selector]),
+                                    { enum: options })),
+                  { type: 'object' });
+};

lib/fields/checkbox.js
@@ -1,0 +1,24 @@
+'use strict';
+
+var React = require('react');
+var $ = React.DOM;
+
+
+var CheckBox = React.createClass({
+  displayName: 'CheckBox',
+
+  handleChange: function(event) {
+    var val = event.target.checked;
+    this.props.update(this.props.path, val, val);
+  },
+  render: function() {
+    return $.input({
+      name: this.props.key,
+      type: "checkbox",
+      checked: this.props.value || false,
+      onChange: this.handleChange });
+  }
+});
+
+module.exports = CheckBox;
+

lib/fields/file.js
@@ -1,0 +1,56 @@
+'use strict';
+
+var React = require('react');
+var $ = React.DOM;
+
+var ou = require('plexus-objective');
+
+var types = require('../types');
+var wrapped = require('./utils/wrapped');
+
+
+var FileField = React.createClass({
+  displayName: 'FileField',
+
+  loadFile: function(event) {
+    var reader = new FileReader();
+    var file = event.target.files[0];
+    var val = ou.merge(this.props.getValue(this.props.path), {
+      name: file.name,
+      type: file.type,
+      size: file.size
+    });
+
+    this.props.update(this.props.path, val, val);
+
+    reader.onload = function(event) {
+      val.data = event.target.result;
+      this.props.update(this.props.path, val, val);
+    }.bind(this);
+
+    if (file) {
+      if (this.props.mode === 'dataURL') {
+        reader.readAsDataURL(file);
+      }
+      else {
+        reader.readAsText(file);
+      }
+    }
+  },
+  render: function() {
+    var fields = this.props.fields || {};
+    var value = this.props.value || {};
+    var list = [
+      $.input({ key: "input", type: "file", onChange: this.loadFile }),
+      $.dl({ key: "fileProperties" },
+           $.dt(null, "Name"), $.dd(null, value.name || '-'),
+           $.dt(null, "Size"), $.dd(null, value.size || '-'),
+           $.dt(null, "Type"), $.dd(null, value.type || '-'))
+    ];
+
+    return wrapped.section(this.props, list.concat(types.object(fields, this.props)));
+  }
+});
+
+module.exports = FileField;
+

lib/fields/index.js
@@ -1,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    CheckBox: require('./checkbox'),
+    FileField: require('./file'),
+    InputField: require('./input'),
+    UserDefinedField: require('./user_defined'),
+    Selection: require('./selection'),
+    make: require('./make'),
+};

lib/fields/input.js
@@ -1,0 +1,39 @@
+'use strict';
+
+var React = require('react');
+var $ = React.DOM;
+
+var normalizer = require('./utils/normalizer');
+var parser = require('./utils/parser');
+
+
+var InputField = React.createClass({
+  displayName: 'InputField',
+
+  normalize: function(text) {
+    return normalizer[this.props.type](text);
+  },
+  parse: function(text) {
+    return parser[this.props.type](text);
+  },
+  handleChange: function(event) {
+    var text = this.normalize(event.target.value);
+    this.props.update(this.props.path, text, this.parse(text));
+  },
+  handleKeyPress: function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  },
+  render: function() {
+    return $.input({
+      type      : "text",
+      name      : this.props.key,
+      value     : this.props.value || '',
+      onKeyPress: this.handleKeyPress,
+      onChange  : this.handleChange });
+  }
+});
+
+module.exports = InputField;
+

lib/fields/make.js
@@ -1,0 +1,61 @@
+'use strict';
+
+var React = require('react');
+
+var ou = require('plexus-objective');
+
+var resolve = require('../resolve');
+var types = require('../types');
+var wrapped = require('./utils/wrapped');
+
+
+module.exports = function(fields, props) {
+  var schema = resolve(props.schema, props.context);
+  var hints = schema['x-hints'] || {};
+  var inputComponent = ou.getIn(hints, ['form', 'inputComponent']);
+
+  props = ou.merge(props, {
+    schema: schema,
+    key   : makeKey(props.path),
+    value : props.getValue(props.path),
+    errors: props.getErrors(props.path),
+    type  : schema.type
+  });
+
+  if (inputComponent) {
+    props = ou.merge(props, { component: props.handlers[inputComponent] });
+    return wrapped.field(props, React.createElement(fields.UserDefinedField, props));
+  } else if (hints.fileUpload) {
+    // FileField cannot depend on fields directly (cyclic dependency)
+    props = ou.merge(props, { fields: fields });
+    return React.createElement(
+      fields.FileField, ou.merge(props, { mode: hints.fileUpload.mode }));
+  }
+  else if (schema['oneOf']) {
+    return wrapped.section(props, types.alternative(fields, props));
+  }
+  else if (schema['enum']) {
+    props = ou.merge(props, {
+        values: schema['enum'],
+        names: schema['enumNames'] || schema['enum'] });
+    return wrapped.field(props, React.createElement(fields.Selection, props));
+  }
+
+  switch (schema.type) {
+  case "boolean":
+    return wrapped.field(props, React.createElement(fields.CheckBox, props));
+  case "object" :
+    return wrapped.section(props, types.object(fields, props));
+  case "array"  :
+    return wrapped.section(props, types.array(fields, props));
+  case "number" :
+  case "integer":
+  case "string" :
+  default:
+    return wrapped.field(props, React.createElement(fields.InputField, props));
+  }
+};
+
+function makeKey(path) {
+  return path.join('_');
+}

commit 83c930045ced40d4f5ada1302dd9aaa093843159

Allow errors and description to be undefined
Regression introduced by faaff6e and fix similar to e42725a.

lib/fields/utils/wrapped.js
@@ -16,10 +16,10 @@ var errorClass = function(errors) {
 
 var makeTitle = function(description, errors) {
   var parts = [];
-  if (description !== null && description.length > 0) {
+  if (description && description.length > 0) {
     parts.push(description);
   }
-  if (errors !== null && errors.length > 0) {
+  if (errors && errors.length > 0) {
     parts.push(errors.join('\n'));
   }
   return parts.join('\n\n');