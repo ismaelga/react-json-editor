d6884ab80aaffb5ce6353c3e5e00a588bb20ab00
figo <rui.plus.monteiro@glazedsolutions.com>
2015-12-21 16:47:40 +0000

    Added move method to reorder array items.

--- dist/react-json-editor.js
+++ dist/react-json-editor.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define(["react"], factory);
 	else if(typeof exports === 'object')
-		exports["react-json-editor"] = factory(require("react"));
+		exports["PlexusForm"] = factory(require("react"));
 	else
-		root["react-json-editor"] = factory(root["react"]);
+		root["PlexusForm"] = factory(root["react"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -96,7 +96,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var fields = __webpack_require__(4);
 	var normalise = __webpack_require__(17);
-	var validator = __webpack_require__(18);
 
 
 	module.exports = React.createClass({
@@ -145,8 +144,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return this.state.errors[makeKey(path)];
 	  },
 	  validate: function(schema, values, context) {
-	    var validate = this.props.validate || validator;
-	    return hashedErrors(validate(schema, values, context));
+	    return hashedErrors(this.props.validate(schema, values, context));
 	  },
 	  preventSubmit: function(event) {
 	    event.preventDefault();
@@ -233,15 +231,19 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	/*
 	The MIT License (MIT)
+
 	Copyright (c) 2014 The Australian National University
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -305,25 +307,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	};
 
 
-	var without = function(obj) {
-	  var args = [].slice.call(arguments);
-	  var result = Array.isArray(obj) ? [] : {};
-
-	  for (var key in obj)
-	    if (args.indexOf(key) < 0)
-	      result[key] = obj[key];
-
-	  return result;
-	};
-
-
 	var prune = function(root) {
 	  var result, isArray, key, val
 
 	  if (root == null || root === '')
 	    result = null;
 	  else if (root.constructor === Array || root.constructor === Object) {
-	    isArray = Array.isArray(root);
+	    isArray = Array.isArray(root); 
 	    result = isArray ? [] : {};
 	    for (key in root) {
 	      val = prune(root[key]);
@@ -385,7 +375,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = {
 	  object : object,
 	  merge  : merge,
-	  without: without,
 	  getIn  : getIn,
 	  setIn  : setIn,
 	  prune  : prune,
@@ -425,7 +414,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  displayName: 'CheckBox',
 
 	  handleChange: function(event) {
-	    var val = event.target.checked;
+	    var val = event.target.checked ? true : null;
 	    this.props.update(this.props.path, val, val);
 	  },
 	  render: function() {
@@ -521,12 +510,71 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return types.object(fields, ou.merge(props, { schema: s }));
 	  },
 	  array: function(fields, props) {
+	    var move = function(props, i, n) {
+		  return function(to) {
+	        if(!canMoveUp(i, n) && !canMoveDown(i, n)) return;
+	        var newList = props.getValue(props.path);
+	        var value = newList.splice(to, 1);
+
+	        newList.splice(i, 0, value[0]);
+	        props.update(props.path, newList, newList);
+	      };
+		};
+	    var canMoveUp = function(i, n) {
+	      return i > 0 && i < n - 1;
+	    };
+	    var moveUp = function(props, i, n) {
+	      return function() {
+	        if(!canMoveUp(i, n)) return;
+	        var newList = props.getValue(props.path);
+	        var value = newList.splice(i, 1);
+
+	        newList.splice(i - 1, 0, value[0]);
+	        props.update(props.path, newList, newList);
+	      };
+	    };
+
+	    var canMoveDown = function(i, n) {
+	      return n > 1 && i < n - 2;
+	    };
+	    var moveDown = function(props, i, n) {
+	      return function() {
+	        if(!canMoveDown(i, n)) return;
+	        var newList = props.getValue(props.path);
+	        var value = newList.splice(i, 1);
+
+	        newList.splice(i + 1, 0, value[0]);
+	        props.update(props.path, newList, newList);
+	      };
+	    };
+
+	    var canRemoveItem = function(i, n) {
+	      return i < n;
+	    };
+
+	    var removeItem = function(props, i, n) {
+	      return function() {
+	        if(!canRemoveItem(i, n)) return;
+
+	        var newList = props.getValue(props.path);
+	        newList.splice(i, 1);
+	        props.update(props.path, newList, newList);
+	      };
+	    };
+
 	    var n = (props.getValue(props.path) || []).length + 1;
 	    var list = [];
 	    for (var i = 0; i < n; ++i) {
 	      list.push(fields.make(fields, ou.merge(props, {
-	        schema: props.schema.items,
-	        path  : props.path.concat(i),
+	        schema       : props.schema.items,
+	        path         : props.path.concat(i),
+	        move         : move(props, i, n),
+	        moveUp       : moveUp(props, i, n),
+	        moveDown     : moveDown(props, i, n),
+	        canMoveUp    : canMoveUp(i, n),
+	        canMoveDown  : canMoveDown(i, n),
+	        removeItem   : removeItem(props, i, n),
+	        canRemoveItem: canRemoveItem(i, n)
 	      })));
 	    }
 
@@ -721,21 +769,51 @@ return /******/ (function(modules) { // webpackBootstrap
 	});
 
 
-	var propsForWrapper = function(props) {
-	  return {
+	var propsForWrapper = function(props, section) {
+	  var propsFW = {
 	    label      : props.label,
 	    path       : props.path,
 	    errors     : props.errors,
 	    classes    : ou.getIn(props.schema, ['x-hints', 'form', 'classes']),
 	    title      : props.schema.title,
 	    type       : props.schema.type,
-	    description: props.schema.description
+	    description: props.schema.description,
+	    schema     : props.schema
 	  };
+
+	  if(section && props.isArrayItem) {
+	    if(props.isArrayItem) {
+	      var vals = props.getValue(props.path);
+	      var title = props.title;
+	      if(vals) {
+	        var itemHeaderKey = ou.getIn(props.schema, ['x-hints', 'itemHeader', "property"]);
+	        var itemHeader = (itemHeaderKey && vals[itemHeaderKey]) || vals.title || vals.name;
+	        title = title && itemHeader ? title + " - " + itemHeader : itemHeader || title;
+	      }
+
+	      propsFW = ou.merge(propsFW, {
+	        title        : title || propsFW.title,
+	        move         : props.move,
+	        moveUp       : props.moveUp,
+	        moveDown     : props.moveDown,
+	        canMoveUp    : props.canMoveUp,
+	        canMoveDown  : props.canMoveDown,
+	        removeItem   : props.removeItem,
+	        canRemoveItem: props.canRemoveItem
+	      });
+	    }
+
+	    propsFW = ou.merge(propsFW, {
+	      isArrayItem: props.isArrayItem
+	    });
+	  }
+
+	  return propsFW;
 	};
 
 	exports.section = function(props, fields) {
 	  return React.createElement(props.sectionWrapper || SectionWrapper,
-	    propsForWrapper(props),
+	    propsForWrapper(props, true),
 	    fields);
 	};
 
@@ -968,6 +1046,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	    type  : schema.type
 	  });
 
+	  if(props.moveUp !== undefined) {
+	    if(props.isArrayItem) {
+	      delete(props.isArrayItem);
+	      delete(props.canMoveUp);
+	      delete(props.canMoveDown);
+	      delete(props.move);
+	      delete(props.moveUp);
+	      delete(props.moveDown);
+	    } else {
+	      props.isArrayItem = true;
+	    }
+	  }
+
 	  if (inputComponent) {
 	    props = ou.merge(props, { component: props.handlers[inputComponent] });
 	    return wrapped.field(props, React.createElement(fields.UserDefinedField, props));
@@ -1054,256 +1145,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 	  return result;
 	}
-
-
-/***/ },
-/* 18 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/*
-	The MIT License (MIT)
-	Copyright (c) 2014 The Australian National University
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-	*/
-
-	'use strict';
-
-	var ou = __webpack_require__(3);
-
-	var checkNumber = function(schema, instance) {
-	  var errors = [];
-
-	  if (schema.maximum !== null) {
-	    if (instance > schema.maximum)
-	      errors.push('may be at most ' + schema.maximum);
-	    else if (schema.exclusiveMaximum && instance >= schema.maximum)
-	      errors.push('must be less than ' + schema.maximum);
-	  }
-	  if (schema.minimum !== null) {
-	    if (instance < schema.minimum)
-	      errors.push('must be at least ' + schema.minimum);
-	    else if (schema.exclusiveMinimum && instance <= schema.minimum)
-	      errors.push('must be more than ' + schema.minimum);
-	  }
-	  if (schema.multipleOf != null) {
-	    if ((instance / schema.multipleOf) % 1 != 0)
-	      errors.push('must be a multiple of ' + schema.multipleOf);
-	  }
-
-	  return errors;
-	};
-
-
-	var fieldErrors = function(errors) {
-	  if (errors.length > 0)
-	    return [ { path: [], errors: errors } ];
-	  else
-	    return [];
-	};
-
-
-	var validator = {};
-
-
-	validator.boolean = function(schema, instance) {
-	  var errors = [];
-
-	  if (typeof instance != 'boolean')
-	    errors.push('must be boolean');
-
-	  return fieldErrors(errors);
-	};
-
-
-	validator.enum = function(schema, instance) {
-	  var errors = [];
-
-	  if (schema.enum.indexOf(instance) < 0)
-	    errors.push('value not in list');
-
-	  return fieldErrors(errors);
-	};
-
-
-	validator.number = function(schema, instance) {
-	  var errors = [];
-
-	  if (typeof instance != 'number')
-	    errors.push('must be a number');
-	  else
-	    errors = checkNumber(schema, instance);
-
-	  return fieldErrors(errors);
-	};
-
-
-	validator.integer = function(schema, instance) {
-	  var errors = [];
-
-	  if (typeof instance != 'number')
-	    errors.push('must be a number');
-	  else {
-	    errors = checkNumber(schema, instance);
-	    if (instance % 1 > 0)
-	      errors.unshift('must be an integer');
-	  }
-
-	  return fieldErrors(errors);
-	};
-
-
-	validator.string = function(schema, instance) {
-	  var errors = [];
-
-	  if (typeof instance != 'string')
-	    errors.push('must be a string');
-	  else {
-	    if (schema.maxLength != null && instance.length > schema.maxLength)
-	      errors.push('may have at most ' + schema.maxLength + ' characters');
-	    if (schema.minLength != null && instance.length < schema.minLength)
-	      errors.push('must have at least ' + schema.minLength + ' characters');
-	    if (schema.pattern != null && !(RegExp(schema.pattern).test(instance)))
-	      errors.push('must match ' + schema.pattern);
-	  }
-
-	  return fieldErrors(errors);
-	};
-
-
-	validator.array = function(schema, instance, context) {
-	  var errors = [];
-	  var result, i, j;
-
-	  if (!Array.isArray(instance))
-	    return fieldErrors(['must be an array']);
-	  else {
-	    if (schema.maxItems != null && instance.length > schema.maxItems)
-	      errors.push('may have at most ' + schema.maxItems + ' items');
-	    if (schema.minItems != null && instance.length < schema.minItems)
-	      errors.push('must have at least ' + schema.minItems + ' items');
-	    result = fieldErrors(errors);
-
-	    if (schema.items != null) {
-	      for (i in instance) {
-	        errors = validate(schema.items, instance[i], context);
-	        for (j in errors) {
-	          result.push({
-	            path  : [i].concat(errors[j].path),
-	            errors: errors[j].errors
-	          });
-	        }
-	      }
-	    }
-	  }
-
-	  return result;
-	};
-
-
-	var requires = function(schema, key) {
-	  var subschema;
-
-	  if (schema.required != null && schema.required.indexOf(key) >= 0)
-	    return 'must be present';
-	  else {
-	    subschema = schema.properties[key];
-	    if (subschema.type == 'array' && subschema.minItems > 0)
-	      return 'must have at least ' + subschema.minItems + ' items';
-	    else
-	      return null;
-	  }
-	};
-
-	validator.object = function(schema, instance, context) {
-	  var result = [];
-	  var key, errors, i;
-
-	  if (instance == null)
-	    instance = {};
-
-	  if (instance.constructor !== Object)
-	    result.push({ path: [], errors: ['must be a plain object'] });
-	  else {
-	    for (key in schema.properties) {
-	      if (instance.hasOwnProperty(key)) {
-	        errors = validate(schema.properties[key], instance[key], context);
-	        for (i = 0; i < errors.length; ++i)
-	          result.push({
-	            path  : [key].concat(errors[i].path),
-	            errors: errors[i].errors
-	          });
-	      }
-	      else if (requires(schema, key)) {
-	        result.push({
-	          path  : [key],
-	          errors: [requires(schema, key)]
-	        });
-	      }
-	    }
-	  }
-
-	  return result;
-	};
-
-
-	var cat = function(arrayOfArrays) {
-	  return [].concat.apply([], arrayOfArrays);
-	};
-
-
-	var resolve = function(schema, context) {
-	  var reference = schema['$ref'];
-
-	  if (reference) {
-	    if (!reference.match(/^#(\/([a-zA-Z_][a-zA-Z_0-9]*|[0-9]+))*$/))
-	      throw new Error('reference '+reference+' has unsupported format');
-
-	    return {
-	      allOf: [
-	        ou.without(schema, '$ref'),
-	        ou.getIn(context, reference.split('/').slice(1))
-	      ]
-	    };
-	  } else
-	    return schema;
-	};
-
-
-	var validate = function(schema, instance, context) {
-	  var effectiveContext = context || schema;
-	  var effectiveSchema  = resolve(schema, effectiveContext);
-
-	  if (effectiveSchema.allOf) {
-	    var results = [ou.without(effectiveSchema, 'allOf')]
-	      .concat(effectiveSchema.allOf)
-	      .map(function(schema) {
-	        return validate(schema, instance, effectiveContext);
-	      });
-	    return cat(results);
-	  } else {
-	    var type = effectiveSchema.enum ? 'enum' : effectiveSchema.type;
-	    if (type)
-	      return validator[type](effectiveSchema, instance, effectiveContext);
-	    else
-	      return [];
-	  }
-	};
-
-	module.exports = validate;
 
 
 /***/ }
